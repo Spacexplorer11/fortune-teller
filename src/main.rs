5f56296f3220965fe770cb4ee3dc44b9e9a82a20
Akaalroop Singh <akaal@akaalroop.com>
2026-01-11 18:25:36 +0000

    Started the code, simple input NPC stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to FORTUNE TELLER");
+    println!("(Warning: if you use invalid inputs the program will fail)");
+    println!("Here your fortune will be toooolldd. How are you feeling today?");
+    let feeling = input();
+
+    println!("Sooo you are feeling {feeling} are you now?");
+    println!("What's your name dear user?");
+    let name = input();
+
+    println!("Hi {name}!")
+}
+
+fn input() -> String {
+    let mut text = String::new();
+    io::stdin().read_line(&mut text).expect("Some error occurred while taking your input, please restart the program");
+    match text.strip_suffix("\n") {
+        Some(text) => String::from(text),
+        None => panic!("Idk what happened, try again pls")
+    }
 }
